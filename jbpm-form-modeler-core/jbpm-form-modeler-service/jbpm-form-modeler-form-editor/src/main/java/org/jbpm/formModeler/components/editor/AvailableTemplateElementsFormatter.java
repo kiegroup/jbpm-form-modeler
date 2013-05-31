3bfcf80ab088176557e3fe0277779bfe63290d09
nmirasch <nmirasch@redhat.com>
2013-05-31 18:48:31 +0200

    form template added

---
+++ jbpm-form-modeler-core/jbpm-form-modeler-service/jbpm-form-modeler-form-editor/src/main/java/org/jbpm/formModeler/components/editor/AvailableTemplateElementsFormatter.java
@@ -0,0 +1,78 @@
+/**
+ * Copyright (C) 2012 JBoss Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jbpm.formModeler.components.editor;
+
+
+import org.jbpm.formModeler.api.model.Field;
+import org.jbpm.formModeler.api.model.Form;
+import org.jbpm.formModeler.service.LocaleManager;
+import org.jbpm.formModeler.service.bb.mvc.taglib.formatter.Formatter;
+import org.jbpm.formModeler.service.bb.mvc.taglib.formatter.FormatterException;
+
+import javax.inject.Named;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+@Named("AvailableTemplateElementsFormatter")
+public class AvailableTemplateElementsFormatter extends Formatter {
+    private static transient org.apache.commons.logging.Log log = org.apache.commons.logging.LogFactory.getLog(AvailableTemplateElementsFormatter.class.getName());
+
+
+    public FormTemplateEditor getFormTemplateEditor() {
+        return WysiwygFormEditor.lookup().getFormTemplateEditor();
+    }
+
+
+    public void service(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws FormatterException {
+        String typeOfItems = (String) getParameter("type"); //Either Field or Label
+        try {
+            Form form = getFormTemplateEditor().getForm();
+            List fieldsToAdd = new ArrayList();
+            Set fields = form.getFormFields();
+            for (Iterator iterator = fields.iterator(); iterator.hasNext();) {
+                Field formularyField = (Field) iterator.next();
+                fieldsToAdd.add(formularyField);
+            }
+            renderItemsToAdd(fieldsToAdd, typeOfItems);
+        } catch (Exception e) {
+            log.error("Error: ", e);
+        }
+    }
+
+    protected void renderItemsToAdd(List fieldsToAdd, String type) {
+        if (fieldsToAdd != null && !fieldsToAdd.isEmpty()) {
+            renderFragment("outputStartItemsToAdd");
+            for (int i = 0; i < fieldsToAdd.size(); i++) {
+                Field field = (Field) fieldsToAdd.get(i);
+                setAttribute("key", ("Field".equals(type) ? Form.TEMPLATE_FIELD : Form.TEMPLATE_LABEL)
+                        + "{" + field.getFieldName() + "}");
+                setAttribute("val",
+                        field.getFieldName()
+                                + " - " +
+                                field.getLabel().get(LocaleManager.lookup().getDefaultLang())
+                );
+                renderFragment("outputItemToAdd");
+            }
+            renderFragment("outputEndItemsToAdd");
+        }
+    }
+
+
+}
---
+++ jbpm-form-modeler-core/jbpm-form-modeler-service/jbpm-form-modeler-form-editor/src/main/java/org/jbpm/formModeler/components/editor/WysiwygEditFormTemplateFormatter.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright (C) 2012 JBoss Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jbpm.formModeler.components.editor;
+
+import org.apache.commons.logging.Log;
+import org.jbpm.formModeler.service.bb.mvc.taglib.formatter.Formatter;
+import org.jbpm.formModeler.service.bb.mvc.taglib.formatter.FormatterException;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Named("WysiwygEditFormTemplateFormatter")
+public class WysiwygEditFormTemplateFormatter extends Formatter {
+
+    @Inject
+    private Log log;
+
+    public void service(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws FormatterException {
+        try {
+            WysiwygFormEditor editor = WysiwygFormEditor.lookup();
+            FormTemplateEditor templateEditor = editor.getFormTemplateEditor();
+            setAttribute("templateContent", templateEditor.getTemplateContent());
+            setAttribute("templateToLoad", templateEditor.getTemplateToLoad());
+            setAttribute("loadTemplate", Boolean.valueOf(templateEditor.isLoadTemplate()));
+            setAttribute("formId", templateEditor.getFormId());
+            renderFragment("output");
+        }catch (Exception e){
+            log.error("Something unexpected has happened editing Form template", e);
+        }
+    }
+}
