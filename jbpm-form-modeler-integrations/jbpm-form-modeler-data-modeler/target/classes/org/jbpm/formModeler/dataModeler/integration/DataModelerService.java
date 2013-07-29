b80abd891fc57cdfe7d898e295e70d99434572a7
Michael Biarnes Kiefer <mbiarnes@redhat.com>
2013-07-29 15:48:52 +0200

    upgrade to 6.1.0-SNAPSHOT, (uberfire 0.4.0-SNAPSHOT)

---
+++ jbpm-form-modeler-integrations/jbpm-form-modeler-data-modeler/target/classes/org/jbpm/formModeler/dataModeler/integration/DataModelerService.java
@@ -0,0 +1,94 @@
+/**
+ * Copyright (C) 2012 JBoss Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jbpm.formModeler.dataModeler.integration;
+
+import org.guvnor.common.services.project.model.Project;
+import org.guvnor.common.services.project.service.ProjectService;
+import org.jbpm.formModeler.core.config.builders.DataHolderBuilder;
+import org.jbpm.formModeler.api.model.DataHolder;
+import org.jbpm.formModeler.api.model.Form;
+import org.jbpm.formModeler.dataModeler.model.DataModelerDataHolder;
+import org.kie.workbench.common.screens.datamodeller.model.DataModelTO;
+import org.kie.workbench.common.screens.datamodeller.model.DataObjectTO;
+import org.uberfire.backend.vfs.Path;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import java.util.HashMap;
+import java.util.Map;
+
+@ApplicationScoped
+public class DataModelerService implements DataHolderBuilder {
+
+    @Inject
+    private org.kie.workbench.common.screens.datamodeller.service.DataModelerService dataModelerService;
+
+    @Inject
+    private ProjectService projectService;
+
+    @Override
+    public Map getOptions(Object path) {
+        Map result = new HashMap();
+        try {
+            DataModelTO dataModelTO = dataModelerService.loadModel(projectService.resolveProject(((Path) path)));
+            if (dataModelTO != null && dataModelTO.getDataObjects() != null) {
+                String className = "";
+                for (DataObjectTO dataObjectTO : dataModelTO.getDataObjects()) {
+                    className = dataObjectTO.getClassName();
+                    result.put(className, className);
+                }
+            }
+        } catch (Throwable e) {
+            result.put("-", "-");
+        }
+        return result;
+    }
+
+    @Override
+    public DataHolder buildDataHolder(Map<String, Object> config) {
+
+        return createDataHolder(config.get("path"), (String) config.get("id"),(String) config.get("inputId"), (String) config.get("outId"), (String) config.get("value"), (String) config.get("color"));
+    }
+
+    @Override
+    public String getId() {
+        return Form.HOLDER_TYPE_CODE_POJO_DATA_MODEL;
+    }
+
+
+    public DataHolder createDataHolder(Object path, String id, String inputId, String outId, String className, String renderColor) {
+        if (path == null) return new DataModelerDataHolder(id, inputId, outId, className, renderColor);
+        DataObjectTO dO = getDataObject(className, (Path) path);
+        return new DataModelerDataHolder(id, inputId, outId, className, renderColor, dO);
+    }
+
+    @Override
+    public boolean supportsPropertyType(String className, Object path) {
+        return getDataObject(className, (Path) path) != null;
+    }
+
+    protected DataObjectTO getDataObject(String className, Path path) {
+        Project project = projectService.resolveProject(((Path) path));
+
+        DataModelTO dataModelTO = dataModelerService.loadModel(project);
+        return dataModelTO.getDataObjectByClassName(className);
+    }
+
+    @Override
+    public int getPriority() {
+        return 2;
+    }
+}
---
+++ jbpm-form-modeler-integrations/jbpm-form-modeler-form-provider/target/classes/org/jbpm/formModeler/kie/services/form/provider/FormModelerFormProvider.java
@@ -0,0 +1,147 @@
+package org.jbpm.formModeler.kie.services.form.provider;
+
+import org.apache.commons.logging.Log;
+import org.jbpm.formModeler.api.client.FormRenderContext;
+import org.jbpm.formModeler.api.client.FormRenderContextManager;
+import org.jbpm.formModeler.api.model.Form;
+import org.jbpm.formModeler.core.config.FormSerializationManager;
+import org.jbpm.kie.services.api.RuntimeDataService;
+import org.jbpm.kie.services.impl.form.FormProvider;
+import org.jbpm.kie.services.impl.model.ProcessDesc;
+import org.kie.api.task.model.Task;
+
+import javax.inject.Inject;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import org.kie.internal.task.api.model.InternalTask;
+
+public class FormModelerFormProvider implements FormProvider {
+    @Inject
+    protected Log log;
+
+    @Inject
+    private RuntimeDataService dataService;
+
+    @Inject
+    private FormSerializationManager formSerializationManager;
+
+    @Inject
+    private FormRenderContextManager formRenderContextManager;
+
+    @Override
+    public int getPriority() {
+        return 2;
+    }
+
+    @Override
+    public String render(String name, ProcessDesc process, Map<String, Object> renderContext) {
+        InputStream template = null;
+        if (process.getForms().containsKey(process.getId())) {
+            template = new ByteArrayInputStream(process.getForms().get(process.getId()).getBytes());
+        } else if (process.getForms().containsKey(process.getId() + "-taskform.form")) {
+            template = new ByteArrayInputStream(process.getForms().get(process.getId() + "-taskform.form").getBytes());
+        }
+
+        if (template == null) return null;
+
+        return renderProcessForm(process, template, renderContext);
+    }
+
+    @Override
+    public String render(String name, Task task, ProcessDesc process, Map<String, Object> renderContext) {
+        InputStream template = null;
+        if(task != null && process != null){
+            String lookupName = "";
+            String formName = ((InternalTask)task).getFormName();
+            if(formName != null && !formName.equals("")){
+                lookupName = formName;
+            }else{
+                lookupName = task.getNames().get(0).getText();
+                
+            }
+            if (process.getForms().containsKey(lookupName)) {
+                template = new ByteArrayInputStream(process.getForms().get(lookupName).getBytes());
+            } else if (process.getForms().containsKey(lookupName.replace(" ", "")+ "-taskform.form")) {
+                template = new ByteArrayInputStream(process.getForms().get(lookupName.replace(" ", "") + "-taskform.form").getBytes());
+            }
+        }
+
+        if (template == null) return null;
+
+        return renderTaskForm(task, template, renderContext);
+    }
+
+    protected String renderTaskForm(Task task, InputStream template, Map<String, Object> renderContext) {
+        String result = null;
+        try {
+            Form form = formSerializationManager.loadFormFromXML(template);
+
+            Map inputs = new HashMap();
+
+            Map outputs = (Map) renderContext.get("outputs");
+
+            Map m = (Map) renderContext.get("inputs");
+            if (m != null) inputs.putAll(m);
+
+            inputs.put("task", task);
+
+            // Adding forms to context while forms are'nt available on marshaller classloader
+            FormRenderContext context = formRenderContextManager.newContext(form, inputs, outputs, buildContextForms(task));
+            context.setMarshaller(renderContext.get("marshallerContext"));
+
+            String status = task.getTaskData().getStatus().name();
+            boolean disabled = "Reserved".equals(status) || "Ready".equals(status);
+            context.setDisabled(disabled);
+            result = context.getUID();
+
+        } catch (Exception e) {
+            log.warn("Error rendering form: ", e);
+        }
+
+        return result;
+    }
+
+    protected String renderProcessForm(ProcessDesc process, InputStream template, Map<String, Object> renderContext) {
+        String result = null;
+        try {
+            Form form = formSerializationManager.loadFormFromXML(template);
+
+            Map ctx = new HashMap();
+
+            ctx.put("process", process);
+
+            // Adding forms to context while forms are'nt available on marshaller classloader
+            FormRenderContext context = formRenderContextManager.newContext(form, ctx, new HashMap<String, Object>(), buildContextForms(process));
+            context.setMarshaller(renderContext.get("marshallerContext"));
+
+            result = context.getUID();
+        } catch (Exception e) {
+            log.warn("Error rendering form: ", e);
+        }
+
+        return result;
+    }
+
+    protected Map<String, Object> buildContextForms(Task task) {
+        ProcessDesc processDesc = dataService.getProcessById(task.getTaskData().getProcessId());
+        return buildContextForms(processDesc);
+    }
+
+    protected Map<String, Object> buildContextForms(ProcessDesc process) {
+        Map<String, String> forms = process.getForms();
+
+        Map<String, Object> ctxForms = new HashMap<String, Object>();
+
+
+        for (Iterator it = forms.keySet().iterator(); it.hasNext();) {
+            String key = (String) it.next();
+            if (!key.endsWith(".form")) continue;
+            String value = forms.get(key);
+            ctxForms.put(key, value);
+        }
+        return ctxForms;
+    }
+}
