4274cce2c11fa1a9042e268b22a69a2b6c8d8cb5
Walter Medvedeo <wmedvedeo@gmail.com>
2013-07-29 11:29:00 +0200

    Form change handler processing control added Old default formulas removed

--- jbpm-form-modeler-core/jbpm-form-modeler-service/jbpm-form-modeler-ui/src/main/java/org/jbpm/formModeler/core/processing/formProcessing/Functions.java
+++ jbpm-form-modeler-core/jbpm-form-modeler-service/jbpm-form-modeler-ui/src/main/java/org/jbpm/formModeler/core/processing/formProcessing/Functions.java
@@ -15,19 +15,16 @@
  */
 package org.jbpm.formModeler.core.processing.formProcessing;
 
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang.WordUtils;
 import org.apache.commons.logging.Log;
 import org.jbpm.formModeler.core.config.FormManager;
 import org.jbpm.formModeler.service.LocaleManager;
-import org.apache.commons.lang.StringUtils;
-import org.apache.commons.lang.WordUtils;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
-import java.math.BigInteger;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
-import java.util.regex.Pattern;
 
 /**
  * Util functions that can be used on field Formulas.
@@ -41,7 +38,6 @@ public class Functions {
     @Inject
     private FormManager formManager;
 
-    private static int[] PESO_DIGITOS_CCC = {1, 2, 4, 8, 5, 10, 9, 7, 3, 6};
     private static String[] MONTHS = {"jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"};
 
     /**
@@ -55,207 +51,6 @@ public class Functions {
     public static final WordUtils Word = new WordUtils();
 
     public Functions() {
-    }
-
-    // Extend this class providing desired project functions
-    public boolean checkNIF(String dni) {
-        if (StringUtils.isBlank(dni)) return false;
-        // Make case insensitive
-        String _dni= dni.toUpperCase();
-        char[] letter = {'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X',
-                'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'};
-        char[] nif = new char[9];
-        boolean startsWithLetter = false;
-        String nifValue;
-
-        if (_dni.length() < 8 || _dni.length() > 9) {
-            return false;
-        }
-        if (_dni.length() == 9) {
-            nif = _dni.toCharArray();
-        } else {
-            nif = new String("0" + _dni).toCharArray();
-        }
-
-        if (!Character.isLetter(nif[8])) {
-            return false;
-        }
-
-        if (nif[0] == 'K' || nif[0] == 'L' || nif[0] == 'M' || nif[0] == 'X' || nif[0] == 'Y' || nif[0] == 'Z') {
-            startsWithLetter = true;
-        } else {
-            if (!Character.isDigit(nif[0])) {
-                return false;
-            }
-        }
-
-        for (int i = 1; i < 8; i++) {
-            if (!Character.isDigit(nif[i])) {
-                return false;
-            }
-        }
-
-        if (startsWithLetter) {
-            nifValue = _dni.substring(1, _dni.length() - 1);
-        } else {
-            nifValue = _dni.substring(0, _dni.length() - 1);
-        }
-
-        int control = Integer.parseInt(nifValue) % 23;
-        return (nif[8] == letter[control]);
-    }
-
-
-    //IBAN         BANCO       OFICINA   CONTROL    NumCuenta
-    // X X X X     X X X X     X X X X    X X    X X X X X X X X X X
-    // 0 1 2 3     4 5 6 7     8 9 0 1    2 3    4 5 6 7 8 9 0 1 2 3
-    public boolean checkIBAN(String numIBAN) {
-        int DC1 = 0, DC2 = 0;
-
-        if (numIBAN.length() != 24) return false;
-
-        String idBancoOficina = numIBAN.substring(4, 12);
-        String numCuenta = numIBAN.substring(14, 24);
-        String CControl = numIBAN.substring(12, 14);
-        String IBAN = numIBAN.substring(0, 4).toUpperCase();
-
-        if (!idBancoOficina.matches("[0-9]{8}") || !numCuenta.matches("[0-9]{10}")
-                || !CControl.matches("[0-9]{2}") || !IBAN.matches("ES([0-9]{2})")) return false;
-
-        int digito;
-        int x = 7;
-        while (x >= 0) {
-            digito = new Integer(idBancoOficina.substring(x, x + 1)).intValue();
-            DC1 = DC1 + (PESO_DIGITOS_CCC[x + 2] * digito);
-            x--;
-        }
-        DC1 = 11 - (DC1 % 11);
-        if (DC1 == 10) DC1 = 1;
-        if (DC1 == 11) DC1 = 0; // Digito control entidad-oficina
-
-        x = 9;
-        while (x >= 0) {
-            digito = new Integer(numCuenta.substring(x, x + 1)).intValue();
-            DC2 = DC2 + (PESO_DIGITOS_CCC[x] * digito);
-            x--;
-        }
-        DC2 = 11 - (DC2 % 11);
-        if (DC2 == 10) DC2 = 1;
-        if (DC2 == 11) DC2 = 0; // Digito control numero C/C
-
-        if (!CControl.equals(new Integer(DC1).toString() + new Integer(DC2).toString())) return false;
-
-        /* Table mapeo letras a numeros para codigo pais, Solo implementado para IBAN Espa�ol
-         A=10	G=16	M=22 	S=28	Y=34
-         B=11	H=17	N=23 	T=29	Z=35
-        C=12	I=18	O=24	U=30
-        D=13	J=19	P=25	V=31
-        E=14	K=20	Q=26	W=32
-        F=15	L=21	R=27	X=33   */
-        String numeroIban = IBAN.replaceAll("E", "14");
-        numeroIban = numeroIban.replaceAll("S", "28");
-        String calcIban = idBancoOficina + CControl + numCuenta + numeroIban;
-        BigInteger result = new BigInteger(calcIban).mod(new BigInteger("97"));
-
-        if (result.compareTo(new BigInteger("1")) != 0) return false;
-
-
-        return true;
-    }
-
-    // CCC Example: 0072 0101 93 0000122351
-    public boolean checkCCC(String CCC) {
-        int DC1 = 0, DC2 = 0;
-
-        if (CCC.length() != 20) return false;
-
-        String idBancoOficina = CCC.substring(0, 8);
-        String numCuenta = CCC.substring(10, 20);
-        String CControl = CCC.substring(8, 10);
-
-
-        if (!idBancoOficina.matches("[0-9]{8}") || !numCuenta.matches("[0-9]{10}")
-                || !CControl.matches("[0-9]{2}")) return false;
-
-        int digito;
-        int x = 7;
-        while (x >= 0) {
-            digito = new Integer(idBancoOficina.substring(x, x + 1)).intValue();
-            DC1 = DC1 + (PESO_DIGITOS_CCC[x + 2] * digito);
-            x--;
-        }
-        DC1 = 11 - (DC1 % 11);
-        if (DC1 == 10) DC1 = 1;
-        if (DC1 == 11) DC1 = 0; // Digito control entidad-oficina
-
-        x = 9;
-        while (x >= 0) {
-            digito = new Integer(numCuenta.substring(x, x + 1)).intValue();
-            DC2 = DC2 + (PESO_DIGITOS_CCC[x] * digito);
-            x--;
-        }
-        DC2 = 11 - (DC2 % 11);
-        if (DC2 == 10) DC2 = 1;
-        if (DC2 == 11) DC2 = 0; // Digito control numero C/C
-
-        if (!CControl.equals(new Integer(DC1).toString() + new Integer(DC2).toString())) return false;
-
-        return true;
-    }
-
-    // CIF Example: A58818501
-    public boolean checkCIF(String CIF) {
-        if (CIF == null) return true;
-
-        CIF = CIF.toUpperCase();
-        Pattern cifPattern = Pattern.compile("[[A-H][J-N][P-S]UVW][0-9]{7}[0-9A-J]");
-        if (!cifPattern.matcher(CIF).matches()) return false;
-
-        int parA = 0;
-        for (int i = 2; i < 8; i += 2) {
-            int digito = Character.digit(CIF.charAt(i), 10);
-            if (digito < 0) return false;
-
-            parA += digito;
-        }
-
-        int nonB = 0;
-        for (int i = 1; i < 9; i += 2) {
-            int digito = Character.digit(CIF.charAt(i), 10);
-            if (digito < 0) return false;
-
-            int nn = 2 * digito;
-            if (nn > 9) nn = 1 + (nn - 10);
-            nonB += nn;
-        }
-
-        int parcialC = parA + nonB;
-        int digitoE = parcialC % 10;
-        int digitoD = (digitoE > 0) ? (10 - digitoE) : 0;
-        char letraIni = CIF.charAt(0);
-        char caracterFin = CIF.charAt(8);
-
-        String CONTROL_SOLO_NUMEROS = "ABEH"; // Sólo admiten números como caracter de control
-        String CONTROL_SOLO_LETRAS = "KPQS"; // Sólo admiten letras como caracter de control
-        String CONTROL_NUMERO_A_LETRA = "JABCDEFGHI"; // Conversión de dígito a letra de control.
-        boolean letterOk = (CONTROL_SOLO_NUMEROS.indexOf(letraIni) < 0 && CONTROL_NUMERO_A_LETRA.charAt(digitoD) == caracterFin);
-        boolean digitOk = (CONTROL_SOLO_LETRAS.indexOf(letraIni) < 0 && digitoD == Character.digit(caracterFin, 10));
-        return letterOk || digitOk;
-    }
-
-    public int yearsOld(Date birthDate) throws ParseException {
-        GregorianCalendar bd = new GregorianCalendar();
-        bd.clear();
-        bd.setTime(birthDate);
-        GregorianCalendar td = new GregorianCalendar();
-        td.clear();
-        td.setTime(new Date());
-        int i = -1;
-        while (td.after(bd)) {
-            i++;
-            bd.add(Calendar.YEAR, 1);
-        }
-        return i;
     }
 
     public Map getYearsBetween(int min, int max) throws Exception {
@@ -366,21 +161,6 @@ public class Functions {
     public String str(String s) {
         return s;
     }
-
-    /**
-     * Returns a color depending on the supplied values.
-     *
-     * @param value value to consider
-     * @param low   lower limit
-     * @param high  upper limit
-     * @return green, orange or red, depending on the
-     */
-    public String semaphor(Number value, Number low, Number high) {
-        if (value.doubleValue() < low.doubleValue()) return "green";
-        if (value.doubleValue() > high.doubleValue()) return "red";
-        return "orange";
-    }
-
 
 }
 
