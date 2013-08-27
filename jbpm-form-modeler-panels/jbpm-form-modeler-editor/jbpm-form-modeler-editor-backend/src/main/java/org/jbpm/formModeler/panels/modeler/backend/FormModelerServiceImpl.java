4d60e5a0b669e7dde80e72540bdc34a4b8f009a0
pefernan <pere.fernandez@gmail.com>
2013-08-27 20:12:48 +0200

    BZ-1001184: integrated form editor with contextual menus

--- jbpm-form-modeler-panels/jbpm-form-modeler-editor/jbpm-form-modeler-editor-backend/src/main/java/org/jbpm/formModeler/panels/modeler/backend/FormModelerServiceImpl.java
+++ jbpm-form-modeler-panels/jbpm-form-modeler-editor/jbpm-form-modeler-editor-backend/src/main/java/org/jbpm/formModeler/panels/modeler/backend/FormModelerServiceImpl.java
@@ -179,4 +179,11 @@ public class FormModelerServiceImpl implements FormModelerService, FormEditorCon
         org.kie.commons.java.nio.file.Path kiePath = paths.convert(context);
         return ioService.deleteIfExists(kiePath);
     }
+
+    @Override
+    public boolean deleteForm(Path context) {
+        if (context == null) return false;
+        org.kie.commons.java.nio.file.Path kiePath = paths.convert(context);
+        return ioService.deleteIfExists(kiePath);
+    }
 }
