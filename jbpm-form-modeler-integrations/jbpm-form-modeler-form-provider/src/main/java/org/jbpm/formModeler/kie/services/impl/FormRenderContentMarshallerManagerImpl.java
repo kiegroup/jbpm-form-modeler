f92178ff2e2dd196117f320e3af5eff58b54107e
pefernan <pere.fernandez@gmail.com>
2013-09-02 10:35:19 +0200

    BZ-1002586: Saving the ContentMarshalerContext on a custom component to avoid serialization conflicts on eap.

---
+++ jbpm-form-modeler-integrations/jbpm-form-modeler-form-provider/src/main/java/org/jbpm/formModeler/kie/services/impl/FormRenderContentMarshallerManagerImpl.java
@@ -0,0 +1,27 @@
+package org.jbpm.formModeler.kie.services.impl;
+
+import org.jbpm.formModeler.kie.services.FormRenderContentMarshallerManager;
+import org.kie.internal.task.api.ContentMarshallerContext;
+
+import javax.enterprise.context.ApplicationScoped;
+import java.util.concurrent.ConcurrentHashMap;
+
+@ApplicationScoped
+public class FormRenderContentMarshallerManagerImpl implements FormRenderContentMarshallerManager {
+    private ConcurrentHashMap<String, ContentMarshallerContext> marhsalContexts = new ConcurrentHashMap<String, ContentMarshallerContext>();
+
+    @Override
+    public void addContentMarshaller(String id, ContentMarshallerContext context) {
+        marhsalContexts.put(id, context);
+    }
+
+    @Override
+    public void removeContentMarshaller(String id) {
+        marhsalContexts.remove(id);
+    }
+
+    @Override
+    public ContentMarshallerContext getContentMarshaller(String id) {
+        return marhsalContexts.get(id);
+    }
+}
