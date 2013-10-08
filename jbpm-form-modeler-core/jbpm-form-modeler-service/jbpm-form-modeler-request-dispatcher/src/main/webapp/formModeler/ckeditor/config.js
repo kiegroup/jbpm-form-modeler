98f3d8884052e77d89c15cd886d8801dfa546847
nmirasch <neus.miras@gmail.com>
2013-10-08 10:00:01 +0200

    BZ:1014051 - HTML Editor doesn't pick changes made in the source code view

--- jbpm-form-modeler-core/jbpm-form-modeler-service/jbpm-form-modeler-request-dispatcher/src/main/webapp/formModeler/ckeditor/config.js
+++ jbpm-form-modeler-core/jbpm-form-modeler-service/jbpm-form-modeler-request-dispatcher/src/main/webapp/formModeler/ckeditor/config.js
@@ -12,8 +12,8 @@ CKEDITOR.editorConfig = function( config ) {
 	config.toolbarGroups = [
 		{ name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
 		{ name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
-		{ name: 'links' },
-		{ name: 'insert' },
+	//	{ name: 'links' },
+	//	{ name: 'insert' },
 		{ name: 'forms' },
 		{ name: 'tools' },
 		{ name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
