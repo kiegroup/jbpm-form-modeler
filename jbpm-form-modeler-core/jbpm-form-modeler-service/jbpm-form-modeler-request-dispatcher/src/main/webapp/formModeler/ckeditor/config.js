e4957413ae2f1f4d37082c3924df04672e471de8
nmirasch <neus.miras@gmail.com>
2013-10-14 13:50:53 +0200

    BZ:1014051 - HTML Editor doesn't pick changes made in the source code view

--- jbpm-form-modeler-core/jbpm-form-modeler-service/jbpm-form-modeler-request-dispatcher/src/main/webapp/formModeler/ckeditor/config.js
+++ jbpm-form-modeler-core/jbpm-form-modeler-service/jbpm-form-modeler-request-dispatcher/src/main/webapp/formModeler/ckeditor/config.js
@@ -3,42 +3,48 @@
  * For licensing, see LICENSE.html or http://ckeditor.com/license
  */
 
-CKEDITOR.editorConfig = function( config ) {
-	// Define changes to default configuration here.
-	// For the complete reference:
-	// http://docs.ckeditor.com/#!/api/CKEDITOR.config
+CKEDITOR.editorConfig = function (config) {
+    // Define changes to default configuration here.
+    // For the complete reference:
+    // http://docs.ckeditor.com/#!/api/CKEDITOR.config
 
-	// The toolbar groups arrangement, optimized for two toolbar rows.
-	config.toolbarGroups = [
-		{ name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
-		{ name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
-	//	{ name: 'links' },
-	//	{ name: 'insert' },
-		{ name: 'forms' },
-		{ name: 'tools' },
-		{ name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
-		{ name: 'others' },
-		'/',
-		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
-		{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
-		{ name: 'styles' },
-		{ name: 'colors' },
-		{ name: 'about' }
-	];
+    config.removeButtons = 'Underline,Subscript,Superscript';
 
-	// Remove some buttons, provided by the standard plugins, which we don't
-	// need to have in the Standard(s) toolbar.
-	config.removeButtons = 'Underline,Subscript,Superscript';
+    // Se the most common block elements.
+    config.format_tags = 'p;h1;h2;h3;pre';
 
-	// Se the most common block elements.
-	config.format_tags = 'p;h1;h2;h3;pre';
+    // Make dialogs simpler.
+    config.removeDialogTabs = 'image:advanced;link:advanced';
 
-	// Make dialogs simpler.
-	config.removeDialogTabs = 'image:advanced;link:advanced';
+    config.allowedContent = true;
 
-    config.allowedContent=true;
-
-
+    config.customConfig = '';
+    config.width = '100%';
+    config.height = 500;
+    config.allowedContent = true;
+    config.allowedContext = true;
+    config.baseFloatZIndex = 20000002; // greater than modal dialog's
+    config.resize_enabled = false;
+    config.startupMode = 'wysiwyg';
+    config.startupShowBorders = false;
+    config.startupFocus = true;
+    config.toolbarLocation = 'top';
+    config.toolbarCanCollapse = false;
+    config.toolbarStartupExpanded = true;
+    config.toolbarGroups = [
+        { name:'document', groups:[ 'document', 'doctools' ] },
+        { name:'clipboard', groups:[ 'clipboard', 'undo' ] },
+        { name:'forms' },
+        { name:'tools' },
+        '/',
+        { name:'basicstyles', groups:[ 'basicstyles', 'cleanup' ] },
+        { name:'paragraph', groups:[ 'list', 'indent', 'blocks', 'align' ] },
+        '/',
+        { name:'mode' },
+        { name:'styles' },
+        { name:'colors' },
+        { name:'others' }
+    ];
 
 
 };
