5feb249a8c90e54c7eceec40baa974ec7cbe1f3e
Toni Rikkola <toni@rikkola.net>
2014-07-29 12:33:39 +0300

    BPMSPL-44 : Improve metadata tab

--- jbpm-form-modeler-panels/jbpm-form-modeler-editor/jbpm-form-modeler-editor-client/src/main/java/org/jbpm/formModeler/editor/client/editors/FormModelerPanelPresenter.java
+++ jbpm-form-modeler-panels/jbpm-form-modeler-editor/jbpm-form-modeler-editor-client/src/main/java/org/jbpm/formModeler/editor/client/editors/FormModelerPanelPresenter.java
@@ -148,13 +148,11 @@ public class FormModelerPanelPresenter {
                                      CommonConstants.INSTANCE.MetadataTabTitle() ) {
             @Override
             public void onFocus() {
-                if ( !metadataWidget.isAlreadyLoaded() ) {
                     metadataWidget.showBusyIndicator( CommonConstants.INSTANCE.Loading() );
                     metadataService.call( new MetadataSuccessCallback( metadataWidget,
                                                                        isReadOnly ),
                                           new HasBusyIndicatorDefaultErrorCallback( metadataWidget )
                                         ).getMetadata( path );
-                }
             }
 
             @Override
