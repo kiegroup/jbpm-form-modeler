b6a20814966f2f5a5a932bec7e778686d52a1475
xiabai <xiabai@redhat.com>
2014-08-05 13:40:45 +0800

    BZ1125905: Inject and initialise KieWorkbenchACL

--- jbpm-form-modeler-showcase/src/main/java/org/jbpm/formModeler/client/ShowcaseEntryPoint.java
+++ jbpm-form-modeler-showcase/src/main/java/org/jbpm/formModeler/client/ShowcaseEntryPoint.java
@@ -29,11 +29,16 @@ import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.Element;
 import com.google.gwt.dom.client.Style;
 import com.google.gwt.user.client.ui.RootPanel;
+import org.jboss.errai.common.client.api.Caller;
+import org.jboss.errai.common.client.api.RemoteCallback;
 import org.jboss.errai.ioc.client.api.AfterInitialization;
 import org.jboss.errai.ioc.client.api.EntryPoint;
 import org.jboss.errai.ioc.client.container.IOCBeanDef;
 import org.jboss.errai.ioc.client.container.SyncBeanManager;
 import org.jbpm.formModeler.client.resources.StandaloneResources;
+import org.kie.workbench.common.services.security.KieWorkbenchACL;
+import org.kie.workbench.common.services.security.KieWorkbenchPolicy;
+import org.kie.workbench.common.services.shared.security.KieWorkbenchSecurityService;
 import org.uberfire.client.mvp.AbstractWorkbenchPerspectiveActivity;
 import org.uberfire.client.mvp.ActivityManager;
 import org.uberfire.client.mvp.PlaceManager;
@@ -56,11 +61,23 @@ public class ShowcaseEntryPoint {
     @Inject
     private ActivityManager activityManager;
 
+    @Inject
+    private KieWorkbenchACL kieACL;
+
+    @Inject
+    private Caller<KieWorkbenchSecurityService> kieSecurityService;
+
     @AfterInitialization
     public void startApp() {
-        loadStyles();
-        setupMenu();
-        hideLoadingPopup();
+        kieSecurityService.call( new RemoteCallback<String>() {
+            public void callback( final String str ) {
+                KieWorkbenchPolicy policy = new KieWorkbenchPolicy( str );
+                kieACL.activatePolicy( policy );
+                loadStyles();
+                setupMenu();
+                hideLoadingPopup();
+            }
+        } ).loadPolicy();
     }
 
     private void loadStyles() {
